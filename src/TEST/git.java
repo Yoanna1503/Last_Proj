8105fc7681c24055d45901383e7f7a2c5afaf33c
Yoanna <127404886+Yoanna1503@users.noreply.github.com>
2023-03-09 15:09:08 +0200

    Update git.java

--- src/TEST/git.java
+++ src/TEST/git.java
@@ -4,7 +4,7 @@ public class git {
     public static void main(String [] args){
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
-            //km
+            //km+1+2+3+4
 
 
 
